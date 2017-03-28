39335c173b734e0f267f659751f9c8048a8ff408
Ivan Mondragon <imondragon@umich.edu>
2017-03-28 01:31:45 -0400

    Ready to read from ttyUSB0

--- receiver_code/tests/pc_read_from_usb.c
+++ receiver_code/tests/pc_read_from_usb.c
@@ -18,7 +18,8 @@
         exit(EXIT_FAILURE); \
     } while(0)
 
-void check_opening(int fd);
+void check_opening(int fd, FILE* ttyUSB);
+void create_uidev(int fd, struct uinput_user_dev* uidev, char* name);
 uint8_t bitstring_to_key(char* bitstring);
 void press_key(int fd, struct input_event* ev, int key);
 
@@ -26,36 +27,24 @@ int main(void) {
   struct uinput_user_dev  uidev;
   struct input_event      ev;
 
-  int                     fd, usbTTY, key;
+  int                     fd, key;
   size_t                  len;
   char*                   line = NULL;
-
-  //usbTTY = fopen("/dev/ttyUSB0", "r");
-  //if ( ttyUSB < 0)
-  //  die("error: could not open USB0");
+  FILE*                   ttyUSB;
 
   // /dev/uinput doesn't explicitly say key/mouse/touch
   fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
-  check_opening(fd);
+  ttyUSB = fopen("/dev/ttyUSB0", "r");
+  check_opening(fd, ttyUSB);
 
-  memset(&uidev, 0, sizeof(uidev)); // clear mem here
-  snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "keyboard-input-sample");
-  uidev.id.bustype = BUS_USB; // BUS_ISA, BUS_PCI are tested OK
-  uidev.id.vendor  = 0x1;
-  uidev.id.product = 0x1;
-  uidev.id.version = 1;
+  create_uidev(fd, &uidev, "keyboard-input-injector");
+  sleep(1);
 
-  if(write(fd, &uidev, sizeof(uidev)) < 0)
-    die("error: write");
-
-  if(ioctl(fd, UI_DEV_CREATE) < 0)
-    die("error: ioctl");
-
-  while( getline(&line, &len, stdin) != -1 ) {
+  while( getline(&line, &len, ttyUSB) != -1 ) {
     key = bitstring_to_key(line);
-    printf("Line: %s", line);
-    printf("Key : %d\n", key);
-    sleep(3);
+    //printf("Line: %s", line);
+    //printf("Key : %d\n", key);
+    //sleep(1);
     press_key(fd, &ev, key);
   }
 
@@ -64,16 +53,20 @@ int main(void) {
     die("error: ioctl");
 
   close(fd);
-  //close(ttyUSB);
+  fclose(ttyUSB);
   if (line)
     free(line);
   return 0;
 }
 
-void check_opening(int fd) {
+void check_opening(int fd, FILE* ttyUSB) {
   int i = 0;
+
   if(fd < 0)
     die("error: open");
+
+  if (ttyUSB == NULL)
+    die("error: could not open USB0");
 
   // says key
   if(ioctl(fd, UI_SET_EVBIT, EV_KEY) < 0)
@@ -98,6 +91,21 @@ void check_opening(int fd) {
   }
 }
 
+void create_uidev(int fd, struct uinput_user_dev* uidev, char* name) {
+  memset(uidev, 0, sizeof(*uidev)); // clear mem here
+  snprintf(uidev->name, UINPUT_MAX_NAME_SIZE, name);
+  uidev->id.bustype = BUS_USB; // BUS_ISA, BUS_PCI are tested OK
+  uidev->id.vendor  = 0x1;
+  uidev->id.product = 0x1;
+  uidev->id.version = 1;
+
+  if(write(fd, uidev, sizeof(*uidev)) < 0)
+    die("error: write");
+
+  if(ioctl(fd, UI_DEV_CREATE) < 0)
+    die("error: ioctl");
+}
+
 uint8_t bitstring_to_key(char* bitstring) {
   uint8_t code = (uint8_t) strtol(bitstring, NULL, 2);
   //if (code >> (sizeof(code)*8 - 1)) // if shift was sent
@@ -106,7 +114,7 @@ uint8_t bitstring_to_key(char* bitstring) {
 }
 
 void press_key(int fd, struct input_event* ev, int key) {
-  if (key <= 0) return;
+  if (key == 0) return;
   //bool shift = false;
 
   //if (key >> (sizeof(key)*8 - 1)) {
@@ -114,7 +122,7 @@ void press_key(int fd, struct input_event* ev, int key) {
   //  shift = true;
   //}
 
-  printf("Pressing %d", key);
+  printf("Pressing %d\n", key);
   // press key
   memset(ev, 0, sizeof(struct input_event));
   ev->type = EV_KEY;
